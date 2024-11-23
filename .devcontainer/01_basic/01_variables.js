7ac472cdb181c47f87478079e00eca15165b9d12
Hozefa.Arielwala <65855196+HozefaArielwala@users.noreply.github.com>
2024-11-23 09:31:59 +0000

    firstCommit

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12222;
+let email="hozefa@gmail.com";
+var accountName = "Hozefa"
+accountCity = "Jaipur"
+
+// accountId=121211 // not allowed as the variable is declared with const keyword.
+let email ='scc@gmail.com'
+accountName ='Arielwala'
+accountCIty='Mumbai'
+
+console.table([accountId, accountemail, accountName, accountCity]);

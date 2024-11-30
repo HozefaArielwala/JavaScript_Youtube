047b47a3cfd9e50c2fdad34e8eb33ba6cc0b1f51
Hozefa.Arielwala <65855196+HozefaArielwala@users.noreply.github.com>
2024-11-30 07:52:14 +0000

    datatypes

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,34 @@
+// Primitve datatypes
+// Number 
+// Bigint -large numbers
+// string -- '' or " "
+// boolean - true/false
+// null - standalone value
+// undefined - value is not defined or doesnot exist.
+// Symbol - for unqiueness
+
+let num = 22
+let string1 = "Hozefa"
+let boolean1 = true
+let boolean2 = false
+let state; // here the var is declared but value is not declared upon running it will give Undefined as a output
+
+// typeoff
+console.log(typeof("Hozefa"))
+let age=22
+console.log(age); // number
+/* Note :
+typeof(null) -> object 
+typeof(udenfined) -> undefined
+
+Null is treated as object in javascript
+*/
+let var1 = null
+console.log(var1)// null
+console.log(typeof(var1))// object
+let var2;
+console.log(var2); // undefined
+console.log(typeof(var2)); // undefined
+
+
+
